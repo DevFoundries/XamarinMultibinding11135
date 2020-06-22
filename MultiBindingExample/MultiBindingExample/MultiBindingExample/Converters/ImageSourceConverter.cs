4e03e650c04f25fcf4681eb80954b2f4f58176c0
Wm. Barrett Simms <barrett@wbsimms.com>
2020-06-22 09:02:53 -0500

    Initial revision for 11135

---
+++ MultiBindingExample/MultiBindingExample/MultiBindingExample/Converters/ImageSourceConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MultiBindingExample.Converters
+{
+    public class ImageSourceConverter : IMultiValueConverter, IMarkupExtension
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Concat(values);
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ MultiBindingExample/MultiBindingExample/MultiBindingExample/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MultiBindingExample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Add some data
+            SomeDataSource = new ObservableCollection<SomeDataSource>()
+            {
+                new SomeDataSource()
+                {
+                    SimpleBinding = "First Simple 1",
+                    JoinBinding1 = "First Join 1",
+                    JoinBinding2 = "Second Join 1"
+                },
+                new SomeDataSource()
+                {
+                    SimpleBinding = "First Simple 2",
+                    JoinBinding1 = "First Join 2",
+                    JoinBinding2 = "Second Join 2"
+                },
+                new SomeDataSource()
+                {
+                    SimpleBinding = "First Simple 3",
+                    JoinBinding1 = "First Join 3",
+                    JoinBinding2 = "Second Join 3"
+                }
+            };
+            this.SampleCollectionView.ItemsSource = SomeDataSource;
+        }
+
+
+        public ObservableCollection<SomeDataSource> SomeDataSource { get; set; } = new ObservableCollection<SomeDataSource>();
+
+    }
+
+    public class SomeDataSource
+    {
+        public string SimpleBinding { get; set; }
+        public string JoinBinding1 { get; set; }
+        public string JoinBinding2 { get; set; }
+        public string Url { get; set; } =
+            "i/1000x1000/88/55/sample-rubber-stamp-vector-11858855.jpg";
+        public string Host { get; set; } =
+            "https://cdn1.vectorstock.com/";
+    }
+}
